288de155542d8b087d10ab96cb7866e6273c7bb8
displaytivi <116294781+displaytivi@users.noreply.github.com>
2023-02-07 17:30:11 +0700

    Create drm.js

---
+++ drm/drm.js
@@ -0,0 +1,29 @@
+<div id="player"></div>
+<script type="text/javascript" src="//cdn.dashjs.org/vx.x.x/dash.all.min.js"></script>
+<script>
+var player = new Playerjs({id:"player", file:"//vas-mobiletv2.cdn.mytv.vn/pkg20/wrap/xlive.mpd?c=vstv340&offset=1&nodelay=1", dashsettings:{streaming: {buffer: {fastSwitchEnabled: true}}}});
+{
+
+   "com.widevine.alpha":{
+
+      "serverURL":"URL"
+
+   },
+
+   "com.microsoft.playready":{
+
+     "serverURL":"URL"
+
+   },
+
+   "org.w3.clearkey":{
+
+      "clearkeys":{
+
+         "mzbWz0eVShiutuI5U/VWRw":"PRhSVy5otGoTMh4z2winfA"
+
+      }
+
+   }
+
+</script>
